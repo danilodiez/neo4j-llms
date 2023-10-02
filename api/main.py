cbd9cc9a40deb2245e2d2e5577f56c4450cc492b
Danilo Diez <daniloo10.dd@gmail.com>
2023-10-02 17:36:37 -0300

    feat: expose agents on the api and get chat like interaction

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import GraphCypherQAChain, LLMChain
 from langchain.graphs import Neo4jGraph
@@ -7,6 +8,8 @@ from langchain.agents import AgentType
 from langchain.prompts import MessagesPlaceholder, PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.llms import OpenAI
+from langchain.tools import StructuredTool, BaseTool
+from langchain.callbacks.manager import CallbackManagerForToolRun
 
 from dotenv import load_dotenv
 import os
@@ -22,42 +25,93 @@ graph = Neo4jGraph(
 )
 llm = ChatOpenAI(openai_api_key=OPEN_AI_KEY, temperature=0)
 
+# Agent for querying the database
+CYPHER_GENERATION_TEMPLATE = """
+Task:Generate Cypher statement to query a graph database.
+Instructions:
+Use only the provided relationship types and properties in the schema.
+Do not use any other relationship types or properties that are not provided.
+Schema:
+{schema}
+Cypher examples:
+# How many streamers are from Norway?
+MATCH (s:Stream)-[:HAS_LANGUAGE]->(:Language {{name: 'no'}})
+RETURN count(s) AS streamers
+
+Note: Do not include any explanations or apologies in your responses.
+Do not respond to any questions that might ask anything else than for you to construct a Cypher statement.
+Do not include any text except the generated Cypher statement.
+The question is:
+{question}
+"""
+
+CYPHER_GENERATION_PROMPT = PromptTemplate(
+    input_variables=["schema", "question"], template=CYPHER_GENERATION_TEMPLATE
+)
 cypher_chain = GraphCypherQAChain.from_llm(
-    ChatOpenAI(openai_api_key=OPEN_AI_KEY, temperature=0), graph=graph,
-    verbose=True
+    ChatOpenAI(openai_api_key=OPEN_AI_KEY, temperature=0),
+    graph=graph,
+    verbose=True,
+    cypher_prompt=CYPHER_GENERATION_PROMPT
 )
 
-GENERATE_GRAPH_PROMPT=''' You are a data scientist working for a company that is building a graph database. Your task is to extract information from user {input} and convert it into a graph database.
-Provide a set of Nodes in the form [ENTITY_ID, TYPE, PROPERTIES] and a set of relationships in the form [ENTITY_ID_1, RELATIONSHIP, ENTITY_ID_2, PROPERTIES].
-It is important that the ENTITY_ID_1 and ENTITY_ID_2 exists as nodes with a matching ENTITY_ID. If you can't pair a relationship with a pair of nodes don't add it.
-When you find a node or relationship you want to add try to create a generic TYPE for it that  describes the entity you can also think of it as a label.
-Return it in the form of a Cypher query for creating nodes and relationships
+# Agent for creating/updating the neo4j schema is info not enough
+GENERATE_GRAPH_PROMPT = '''
+    You are a data scientist working for a company that is building a
+    graph database. Your task is to extract information from user affirmations
+    {input} Add this to the schema
+    and convert it into a graph database schema to update the existing neo4j schema.
+    Provide a set of Nodes in the form [ENTITY_ID, TYPE, PROPERTIES]
+    and a set of relationships in the form [ENTITY_ID_1, RELATIONSHIP,
+    ENTITY_ID_2, PROPERTIES].
+    It is important that the ENTITY_ID_1 and ENTITY_ID_2 exists as
+    nodes with a matching ENTITY_ID.
+    If not create those new ENTITIES
+    When you find a node or relationship you want to add try to create a
+    generic TYPE for it that  describes the entity you can also think of
+    it as a label.
+    Return it in the form of a Cypher query for creating nodes and
+    relationships
 '''
 
 cypher_llm = OpenAI(openai_api_key=OPEN_AI_KEY, temperature=0)
-cypher_chain = LLMChain(
+populate_chain = LLMChain(
     llm=cypher_llm,
     prompt=PromptTemplate(
         input_variables=["input"],
-        template=GENERATE_GRAPH_PROMPT)
+        template=GENERATE_GRAPH_PROMPT
+    )
 )
-# cypher_chain.run("Which Deparment has employees?")
+
+class UpdateDBTool(BaseTool):
+    name = "Update-DB"
+    description = "Useful to update neo4j schema with the cypher query"
+
+    def _run(self, query, run_manager = None):
+        """
+        Write the neo4j DB only if the input is in form of a neo4j query
+
+        Args:
+        query: any
+        Returns:
+        None
+        """
+        graph.query(query)
+        graph.refresh_schema()
 
 
 tools = [
     Tool(
         name="Search",
         func=cypher_chain.run,
-        description='''
-         useful for query the ONLY neo4j graph database to answer user questions,
-         if you don't have an answer ask to the user
-        '''
+        description="useful for query neo4j graph database to answer user questions"
     ),
     Tool(
         name="Cypher",
-        func=cypher_chain.run,
+        func=populate_chain.run,
         description="useful for extracting a cypher schema out of text"
-    )
+    ),
+    UpdateDBTool()
 ]
 
 agent_kwargs = {
@@ -78,21 +132,29 @@ agent = initialize_agent(
     memory=memory,
 )
 
-# agent.run("my name is Danilo")
-# agent.run("Could you help me by answering how many sons does Leo Messi Have?")
-# agent.run({
-#     "input": "Convert to cypher: Leo Messi is a football player, and he is married with Antonella Rocuzzo",
-# })
 
-# start asking for the users input??
-response = ""
-while (response != "end"):
-    response = input("What do you want to ask the AI model? \n")
-    agent.run(response)
-
-# app = FastAPI()
+# response = ""
+# while (response != "end"):
+#     response = input("What do you want to ask the AI model? \n")
+#     agent.run(response)
 
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/chat/{input}")
+async def call_chat(input):
+    return {"response": agent.run(input), "graph": graph.schema}
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,32 +3,37 @@ import { OpenAI } from "langchain/llms/openai";
 import "./App.css";
 const OPENAI_KEY = import.meta.env.VITE_OPEN_AI_KEY;
 import { PromptTemplate } from "langchain/prompts";
+import { useChat } from "ai/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const llm = new OpenAI({
-    openAIApiKey: OPENAI_KEY,
-  });
-
-  const prompt = PromptTemplate.fromTemplate(
-    "What is a good name for a company that makes {product}?"
-  );
-
-  const formattedPrompt = async () =>
-    await prompt.format({
-      product: "colorful socks",
-    });
-  const getResult = async () => {
-    const result = await llm.predict(
-      "What would be a good company name for a company that makes colorful socks?"
-    );
-
-    console.log(result);
-    return result;
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  // const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessages(prev => [...prev, input])
+    const response = await fetch('http://127.0.0.1:8000/chat/' + input)
+    const data = await response.json()
+    setMessages(prev => [...prev, data.response])
+    setInput("");
   };
-  getResult();
 
-  return <>hola</>;
+  return (
+    <div>
+    {messages.map((msg, index) => (
+        <div key={index}>
+          {index % 2 === 0 ? "User: " : "AI: "}{msg}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          value={input}
+          placeholder="Say something..."
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default App;
